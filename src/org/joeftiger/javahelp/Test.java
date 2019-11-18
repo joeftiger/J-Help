b3110a88a90948838bcade14d7d6b7a4662182ff
joeftiger <j.oeftiger@protonmail.com>
2019-11-18 13:02:15 +0100

    Added small test method

---
+++ src/org/joeftiger/javahelp/Test.java
@@ -0,0 +1,22 @@
+package org.joeftiger.javahelp;
+
+public class Test {
+	public static void main(String[] args) {
+		Option o1 = new OptionBuilder()
+				.addInvokes("-c", "--count")
+				.setParameterType(Integer.class)
+				.setDescription("This is a very long description intended to fill up the line, such that it will be split up on words accordingly.")
+				.build();
+		Option o2 = new OptionBuilder()
+				.addInvokes("-m", "--method")
+				.addParameters("rank", "random")
+				.setDescription("d".repeat(81 - 24) + " newline")
+				.build();
+
+		Help help = new Help()
+				.setUsage(new Usage().addTargets("foo", "bar"))
+				.addOptions(o1, o2);
+
+		System.out.println(help);
+	}
+}
